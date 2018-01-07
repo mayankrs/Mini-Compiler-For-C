cc5d020c74ae6e4b9bb77691f78901bdca3ab83c
Hetshah1998 <hetshah247@gmail.com>
2018-01-07 15:00:47 +0530

    Add files via upload

---
+++ token.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+    int a,b;
+    printf("Hello!");
+    for ( ; ; ){
+        while(1){
+            break;
+        }
+        continue;
+    }
+    return 0;
+}
