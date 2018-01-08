50fb9026771e70054d0089c3c09afef1fbe128da
Hetshah1998 <hetshah247@gmail.com>
2018-01-08 11:12:55 +0000

    Add files via upload

---
+++ input.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+void func()
+{
+	printf("Trying this out\n");
+}	
+
+int main()
+{
+	int i = 0; //This is new
+	/* printf("\n");
+	printf("\n");*/
+	char x;
+	printf("i=%d\n", i);
+
+	for(;;)
+		break;
+	return 11;
+}
